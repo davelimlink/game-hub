b09d5cb5d4ddd2dc7320bd6a4a6bd5fda1610c0c
LDLcodes <153458380+LDLcodes@users.noreply.github.com>
2024-06-14 14:45:27 +0900

    Build a responsive Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
   return (
     <>
-      <Button colorScheme="blue">Button</Button>
+      <Grid
+        templateAreas={{
+          base: `"nav" "main"`, //The base is for mobile styling. see chakraiu for responsive style
+          lg: `"nav nav" "aside main"`, //The lg: is for large devices.
+        }}
+      >
+        <GridItem area="nav" bg="coral">
+          Nav
+        </GridItem>
+        <Show above="lg">
+          <GridItem area="aside" bg="gold">
+            Aside
+          </GridItem>
+        </Show>
+
+        <GridItem area="main" bg="dodgerblue">
+          Main
+        </GridItem>
+      </Grid>
     </>
   );
 }
