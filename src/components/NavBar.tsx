a8c95da2af1b122249e2e708b975dadc5e040095
LDLcodes <153458380+LDLcodes@users.noreply.github.com>
2024-06-25 15:49:14 +0900

    Filter games by platform

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,17 @@
-import { HStack, Image } from "@chakra-ui/react";
+import { HStack, Image, Text } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
 
 const NavBar = () => {
   return (
     <HStack justifyContent="space-between" padding="10px" marginBottom={2}>
-      <Image src={logo} boxSize="60px" />
-      _<ColorModeSwitch />
+      <HStack>
+        <Image src={logo} boxSize="60px" />
+        <Text fontWeight={"bolder"}>Oreo Gaming</Text>
+      </HStack>
+      <HStack>
+        <ColorModeSwitch />
+      </HStack>
     </HStack>
   );
 };
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -25,7 +25,12 @@ interface FetchGamesResponse {
 }
 */
 
-const useGames = (selectedGenre: Genre | null) => useData<Game>("/games", {params: {genres: selectedGenre?.id}}, [selectedGenre?.id])
+const useGames = (selectedGenre: Genre | null, selectedPlatform: Platform | null) => useData<Game>("/games", {
+  params: {
+    genres: selectedGenre?.id,  
+    parent_platforms: selectedPlatform?.id
+  }}, 
+[selectedGenre?.id, selectedPlatform?.id])
 /*
 {
     const [games, setGames] = useState<Game[]>([]);
